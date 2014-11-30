6f698b3c8ec2321e6b7f196b30e3cbfe3163d2df
Lewis Alva <lalva@tigertext.com>
2014-11-30 11:50:50 -0800

    Delete files that were renamed/not used

--- src/edu/csupomona/cs356/twitter/observer/Observer.java
+++
@@ -1,6 +0,0 @@
-package edu.csupomona.cs356.twitter.observer;
-import edu.csupomona.cs356.twitter.models.TwitterEntity;
-
-public interface Observer {
-  public void update(TwitterEntity twitterEntity);
-}
